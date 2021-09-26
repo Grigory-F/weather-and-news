84b434e48aa1d70bea21e0bde51ad16f57c5f2ef
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2021-09-26 20:20:53 +0300

    www

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    publicPath: '/weather-and-news/',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/',
     css: {
         loaderOptions: {
             sass: {
